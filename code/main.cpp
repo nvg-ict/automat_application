a1d8898887d2527b140f4d8145231fddaee8339f
Nico <116808apd@gmail.com>
2016-12-24 16:06:21 +0100

    Added the main of the application

---
+++ code/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 24 Dec 2016
+ *      Author: Nico
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+	std::cout<<"Program has started"<<std::endl;
+	return 0;
+}
+
+
+
+
