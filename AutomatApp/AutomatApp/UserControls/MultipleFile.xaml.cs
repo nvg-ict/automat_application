248f5da2b18ef18faf959598f943ecd03f335f9e
Nico <116808apd@gmail.com>
2018-03-27 16:13:24 +0200

    New forgotten files. Now we have more working windows

---
+++ AutomatApp/AutomatApp/UserControls/MultipleFile.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutomatApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MultipleFile.xaml
+    /// </summary>
+    public partial class MultipleFile : UserControl
+    {
+        public MultipleFile(int amtOfFiles, int amtOfMatFiles)
+        {
+            InitializeComponent();
+            FileInfo.Content = string.Format("Collected {0} files.", amtOfFiles);
+            MatInfo.Content = string.Format("Collected {0} .mat files.", amtOfMatFiles);
+        }
+    }
+}
---
+++ AutomatApp/AutomatApp/Windows/CustomDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutomatApp.Windows
+{
+    /// <summary>
+    /// Interaction logic for Dialog.xaml
+    /// </summary>
+    public partial class CustomDialog : Window
+    {
+        public CustomDialog(UserControl aUserControl)
+        {
+            InitializeComponent();
+            MainContent.Children.Clear();
+            MainContent.Children.Add(aUserControl);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
